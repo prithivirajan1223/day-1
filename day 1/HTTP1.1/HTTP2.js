db0eb5ab36adf972f1139cf0a666a8dbb2ffc863
Your Name <you@example.com>
2024-02-03 17:16:21 +0530

    first commit

---
+++ day 1/HTTP1.1/HTTP2.js
@@ -0,0 +1,24 @@
+// HTPP MEANS
+         /* HTTP stands for hypertext transfer protocol and it is almost all web application 
+ HTTP is a method computers and servers use to requst and send information . 
+ when someone navigates to cloudflare on their laptop ,thier wed browser sendsan HTTP 
+ Request to the cloud serverfot the contect that appers on page
+
+ //  HTTP/1.1
+             
+        THE first usable version of HTTP Was createdin 1997 .becauseit went through several stages of development . it 
+ first version was HTTP 1.1
+
+ //  HTTP /2
+
+           it was invented in 2015 . it solves several problems that the creatorsof HTTP 1.1 didn't anticipate. HTTTP I
+ is more faster then HTTP 1.1
+
+ //  THE DIFFERENT BETWEEN HTTP1.1 AND HTTP2
+         
+ 1]HTTP2 is faster and more reliable than HTTP1.1.
+ 2]HTTP2 uses multiplexing to avoid network delay, while HTTP1.1 loads a single request for every TCP connection.
+  3]HTTP2 utilizes server pushes to reduce page load time by a greater margin.
+  4]HTTP2 encapsulates all messages in binary format, while HTTP1.1 keeps all requests and responses in plain text format.
+
+ */
